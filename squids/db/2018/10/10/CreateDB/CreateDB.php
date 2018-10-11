54757587b746c1a2ce213ecaa1eee39befcea85c
Marina Sherstuk <marinas@commpeak.com>
2018-10-11 09:53:24 +0300

    Added CreateDB migration

---
+++ squids/db/2018/10/10/CreateDB/CreateDB.php
@@ -0,0 +1,19 @@
+<?php
+namespace SquidMigrationActions_2018_10_10;
+
+
+use Squid\MySql\IMySqlConnector;
+use Squids\Prepared\SimpleAction;
+
+
+class CreateDB extends SimpleAction
+{
+	const ID = '1a6538fef27b0a753fa4fb3e1c4ea7b1';
+	const DEP = [];
+	
+	
+	public function execute(IMySqlConnector $connector)
+	{
+		
+	}
+}
