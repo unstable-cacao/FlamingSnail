b67f05f87321c0a56de64f5e1e0db1eca1a386c9
Alexey Puchkov <alexey.pkv@gmail.com>
2019-02-04 07:06:59 +0200

    Start adding canvas

--- Web/Source/js/FlamingSnail/Actions/RoomAction.js
+++ Web/Source/js/FlamingSnail/Actions/RoomAction.js
@@ -9,7 +9,14 @@ namespace('FlamingSnail.Actions', function (root)
 	
 	RoomAction.prototype.activate = function ()
 	{
-		this.modules('View').render('room')
+		this.modules('View').render('room');
+		
+		var canvas = new fabric.Canvas('screen', {isDrawingMode: true});
+		canvas.freeDrawingBrush.color = '#00000';
+    	canvas.freeDrawingBrush.width = 5;
+    	
+    	canvas.setHeight($(window).height() - 50);
+		canvas.setWidth($(window).width() - 50);
 	};
 	
 	
